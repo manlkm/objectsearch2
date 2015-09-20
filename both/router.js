6cc25b253595e7df1c8f364b6971adc834f340e5
Man Liu <novemliu@gmail.com>
2015-09-20 19:57:24 +0800

    add sth.

--- both/router.js
+++ both/router.js
@@ -2,6 +2,13 @@ Router.configure({
   layoutTemplate: 'layout'
 });
 
+/*Router.route('/item/:_id', function () {
+  this.render('ItemShow');
+ }, {name: 'item.show'
+});*/
+Router.route('/item/:_id', {name: 'item.show'});
+
 Router.map(function() {
 	this.route('index', {path: '/'});
+	this.route('item');
 });
